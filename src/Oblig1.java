0daa73a4b2c43217d07a358488f505077fe8bcf0
dusan <s331367@oslomet.no>
2019-09-15 13:54:50 +0200

    Oppgave 7a, ferdig og ser til å funke!

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,7 +1,40 @@
 public class Oblig1 {
 
+    //Oppgave 7a
+    public static String flett (String s, String t) {
+        //Splitter hvertbokstav fra s og t til en array
+        String[] sSplit = s.split("");
+        String[] tSplit = t.split("");
+
+        //If setning som finner arrayen med størst lengde mellom sSplit og tSplit
+        int lengde=0;
+        if(sSplit.length>tSplit.length){
+            lengde = sSplit.length;
+        } else {
+            lengde = tSplit.length;
+        }
+
+        //For-løkke som setter sammen indeks 0 fra sSplit og tSplit, så indeks 1 i sSplit og tSplit.....
+        //etterhverandre helt til løkken har lest alle indeksene i begge array
+        String ut="";
+        for(int i=0; i<lengde; i++){
+            if(i < sSplit.length && i < tSplit.length) {
+                ut += sSplit[i] + tSplit[i];
+            } else if(i >= sSplit.length){
+                ut += tSplit[i];
+            } else {
+                ut += sSplit[i];
+            }
+        }
+
+        return ut; //Skriver ut resultatet
+    }
+
     public static void main(String[] args){
-        System.out.print("Hei");
+        String a = flett ( "ABC" , "DEFGH" );
+        String b = flett ( "IJKLMN" , "OPQ" );
+        String c = flett ( "" , "AB" );
+        System. out .println(a + " " + b + " " + c);
 
     }
 }
