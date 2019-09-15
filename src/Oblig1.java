3133e90ae193840e4933ef9b40120b8ba4f62802
dusan <s331367@oslomet.no>
2019-09-15 14:31:06 +0200

    Oppgave 8, har starta. Får sortert arrayen i stigende rekkefølge!

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Oblig1 {
 
     //Oppgave 7a
@@ -18,7 +20,7 @@ public class Oblig1 {
         //etterhverandre helt til løkken har lest alle indeksene i begge array
         String ut="";
         for(int i=0; i<lengde; i++){
-            if(i < sSplit.length && i < tSplit.length) {
+            if(i < sSplit.length && i < tSplit.length){
                 ut += sSplit[i] + tSplit[i];
             } else if(i >= sSplit.length){
                 ut += tSplit[i];
@@ -30,11 +32,35 @@ public class Oblig1 {
         return ut; //Skriver ut resultatet
     }
 
+    public static void  indekssortering( int [] a){
+        int[] indeksStigende = {};
+        int tmp;
+
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<a.length-1; j++){
+                    if (a[j + 1] < a[j]) {
+                        tmp = a[j + 1];
+                        a[j + 1] = a[j];
+                        a[j] = tmp;
+                    }
+            }
+
+        }
+
+        for(int k : a) System.out.print(k + " ");
+    }
+
     public static void main(String[] args){
-        String a = flett ( "ABC" , "DEFGH" );
+        /*String a = flett ( "ABC" , "DEFGH" );
         String b = flett ( "IJKLMN" , "OPQ" );
         String c = flett ( "" , "AB" );
-        System. out .println(a + " " + b + " " + c);
+        System. out .println(a + " " + b + " " + c);*/
+
+        int [] a = {6,10,16,11,7,12,3,9,8,5};
+        indekssortering(a);
+
+        // Utskrift: [6, 10, 16, 11, 7, 12, 3, 9, 8, 5] a er ikke endret
+        // Utskrift: [6, 9, 0, 4, 8, 7, 1, 3, 5, 2]
 
     }
 }
